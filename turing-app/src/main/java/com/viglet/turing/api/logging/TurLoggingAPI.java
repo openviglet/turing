78b2faed2fb6147278a660dd8410b9effabb085f
Alexandre Oliveira <alexandre.oliveira@gmail.com>
2025-06-16 10:44:23 -0300

    - MongoDB Properties

--- turing-app/src/main/java/com/viglet/turing/api/logging/TurLoggingAPI.java
+++ turing-app/src/main/java/com/viglet/turing/api/logging/TurLoggingAPI.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,45 +25,50 @@ public class TurLoggingAPI {
     private final boolean enabled;
     private final String connectionString;
     private final String databaseName;
-    private final String generalCollectionName;
+    private final String serverCollectionName;
     private final String indexingCollectionName;
+    private final String aemCollectionName;
 
     public TurLoggingAPI(@Value("${turing.mongodb.enabled:false}") boolean enabled,
-                         @Value("${turing.mongodb.uri:''}") String connectionString,
-                         @Value("${turing.mongodb.logging.database:''}")String databaseName,
-                         @Value("${turing.mongodb.logging.collection.general:''}")String generalCollectionName,
-                         @Value("${turing.mongodb.logging.collection.indexing:''}") String indexingCollectionName) {
+                         @Value("${turing.mongodb.uri:'mongodb://localhost:27017'}") String connectionString,
+                         @Value("${turing.mongodb.logging.database:'turingLog'}") String databaseName,
+                         @Value("${turing.mongodb.logging.collection.server:'server'}") String serverCollectionName,
+                         @Value("${turing.mongodb.logging.collection.indexing:'indexing'}") String indexingCollectionName,
+                         @Value("${turing.mongodb.logging.collection.aem:'aem'}") String aemCollectionName) {
         this.enabled = enabled;
         this.connectionString = connectionString;
         this.databaseName = databaseName;
-        this.generalCollectionName = generalCollectionName;
+        this.serverCollectionName = serverCollectionName;
         this.indexingCollectionName = indexingCollectionName;
+        this.aemCollectionName = aemCollectionName;
     }
 
-    @Operation(summary = "General Logging")
+    @Operation(summary = "Server Logging")
     @GetMapping
-    public List<Document> generalLogging() {
-        List<Document> documentList = new ArrayList<>();
-        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
-            MongoDatabase database = mongoClient.getDatabase(databaseName);
-            MongoCollection<Document> generalCollection = database.getCollection(generalCollectionName);
-             generalCollection.find()
-                    .sort(Sorts.descending(TIMESTAMP))
-                    .limit(100).forEach(documentList::add);
-        } catch (Exception e) {
-          log.error(e.getMessage(), e);
-        }
-        Collections.reverse(documentList);
-        return documentList;
+    public List<Document> serverLogging() {
+        return getDocuments(serverCollectionName);
     }
 
-    @Operation(summary = "General Logging")
+    @Operation(summary = "Indexing Logging")
     @GetMapping("indexing")
     public List<Document> indexingLogging() {
+        return getDocuments(indexingCollectionName);
+    }
+
+    @Operation(summary = "AEM Logging")
+    @GetMapping("aem")
+    public List<Document> aemLogging() {
+        return getDocuments(aemCollectionName);
+    }
+
+    private @NotNull List<Document> getDocuments(String ii) {
         List<Document> documentList = new ArrayList<>();
+        if (!enabled) {
+            return documentList;
+        }
         try (MongoClient mongoClient = MongoClients.create(connectionString)) {
             MongoDatabase database = mongoClient.getDatabase(databaseName);
-            MongoCollection<Document> indexingCollection = database.getCollection(indexingCollectionName);
+            MongoCollection<Document> indexingCollection = database.getCollection(ii);
             indexingCollection.find()
                     .sort(Sorts.descending(TIMESTAMP))
                     .limit(100).forEach(documentList::add);
@@ -72,7 +78,6 @@ public class TurLoggingAPI {
         Collections.reverse(documentList);
         return documentList;
     }
-
 
 
 }
