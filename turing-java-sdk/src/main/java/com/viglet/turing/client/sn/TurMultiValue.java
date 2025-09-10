ac7f1868e220e369c1df46e620aac057ca85e11c
Alexandre Oliveira <alexandre.oliveira@gmail.com>
2025-09-10 18:33:37 -0300

    Refactor TurMultiValue imports and boolean handling
    Optimized import statements in TurMultiValue.java for clarity and consistency. Updated singleItem method to use Boolean object and booleanValue for safer boolean conversion.

--- turing-java-sdk/src/main/java/com/viglet/turing/client/sn/TurMultiValue.java
+++ turing-java-sdk/src/main/java/com/viglet/turing/client/sn/TurMultiValue.java
@@ -1,31 +1,30 @@
 /*
  * Copyright (C) 2016-2022 the original author or authors.
  *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.viglet.turing.client.sn;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+import lombok.Getter;
+import lombok.Setter;
 
 @Setter
 @Getter
@@ -91,9 +90,9 @@ public class TurMultiValue extends ArrayList<String> {
         return singleItem(text, false);
     }
 
-    public static TurMultiValue singleItem(boolean bool, boolean override) {
+    public static TurMultiValue singleItem(Boolean bool, boolean override) {
         TurMultiValue turMultiValue = new TurMultiValue(override);
-        turMultiValue.add(bool ? "true" : "false");
+        turMultiValue.add(bool.booleanValue() ? "true" : "false");
         return turMultiValue;
     }
 
