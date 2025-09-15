cbc4c1366f5a125c4c80f40d01922ed44db59d0e
Alexandre Oliveira <alexandre.oliveira@gmail.com>
2025-09-15 12:50:37 -0300

    Update TurBrowserLauncher.java

--- turing-app/src/main/java/com/viglet/turing/spring/TurBrowserLauncher.java
+++ turing-app/src/main/java/com/viglet/turing/spring/TurBrowserLauncher.java
@@ -6,10 +6,12 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import lombok.extern.slf4j.Slf4j;
 
+@Profile("!test")
 @Slf4j
 @Component
 public class TurBrowserLauncher {
