3278639639b8245d2e984cd28d99b5d315de6fd5
Alexandre Oliveira <alexandre.oliveira@gmail.com>
2025-09-15 12:36:03 -0300

    Migrate config files to YAML and add browser launcher
    Converted application.properties files to application.yaml for both turing-app and turing-connector modules to improve configuration structure and readability. Added TurBrowserLauncher component to turing-app to automatically open the browser on startup.

---
+++ turing-app/src/main/java/com/viglet/turing/spring/TurBrowserLauncher.java
@@ -0,0 +1,31 @@
+package com.viglet.turing.spring;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TurBrowserLauncher {
+    @Value("${turing.url:http://localhost:2700}")
+    private String turingUrl;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void launchBrowser() {
+        System.setProperty("java.awt.headless", "false");
+        if (Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(turingUrl));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Desktop is not supported, cannot open browser automatically.");
+        }
+    }
+}
