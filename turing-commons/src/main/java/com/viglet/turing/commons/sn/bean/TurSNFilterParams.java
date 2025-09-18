2082df73c32496105eee46ff6a5748285a291c3c
Alexandre Oliveira <alexandre.oliveira@gmail.com>
2025-09-18 16:07:21 -0300

    Update README and make TurSNFilterParams Serializable
    README updated for Java 21+, revised build and deployment instructions, and removed outdated links. TurSNFilterParams now implements Serializable with a serialVersionUID for improved compatibility.

--- turing-commons/src/main/java/com/viglet/turing/commons/sn/bean/TurSNFilterParams.java
+++ turing-commons/src/main/java/com/viglet/turing/commons/sn/bean/TurSNFilterParams.java
@@ -1,5 +1,7 @@
 package com.viglet.turing.commons.sn.bean;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Collections;
 import java.util.List;
 import com.viglet.turing.commons.sn.search.TurSNFilterQueryOperator;
@@ -8,7 +10,9 @@ import lombok.Data;
 
 @Builder
 @Data
-public class TurSNFilterParams {
+public class TurSNFilterParams implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
     @Builder.Default
     List<String> defaultValues = Collections.emptyList();
     @Builder.Default
