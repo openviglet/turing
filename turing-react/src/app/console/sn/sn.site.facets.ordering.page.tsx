a1ec64bf98478f8edd109c2117946937e8ffa1b6
Alexandre Oliveira <alexandre.oliveira@gmail.com>
2025-10-27 21:08:00 -0300

    Remove unused state and effect from facet ordering page
    Eliminated the React state and effect logic related to fetching faceted fields, as it was unused. The page now simply renders the header and DraggableTable component.

--- turing-react/src/app/console/sn/sn.site.facets.ordering.page.tsx
+++ turing-react/src/app/console/sn/sn.site.facets.ordering.page.tsx
@@ -1,19 +1,10 @@
 import { DraggableTable } from "@/components/draggable-table";
 import { SubPageHeader } from "@/components/sub.page.header";
-import type { TurSNSiteField } from "@/models/sn/sn-site-field.model";
-import { TurSNSiteService } from "@/services/sn.service";
 import { IconReorder } from "@tabler/icons-react";
-import React from "react";
 import { useParams } from "react-router-dom";
-const turSNSiteService = new TurSNSiteService();
+
 export default function SNSiteFacetOrderingPage() {
   const { id } = useParams() as { id: string };
-  const [snField, setSnField] = React.useState<TurSNSiteField[]>([]);
-  React.useEffect(() => {
-    turSNSiteService.getFacetedFields(id).then(setSnField);
-    console.log("Teste")
-    console.log(snField)
-  }, [id])
   return (
     <><SubPageHeader icon={IconReorder} title="Facet Ordering" description="Order the facets of the search." />
       <DraggableTable id={id} /></>
